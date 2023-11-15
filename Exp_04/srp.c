de8315d28b26bcb93fa5b01b525c12e07097b6ee
Noel Robert <noel060202@gmail.com>
2023-11-15 23:56:43 +0530

    working on shift-reduce parser. logic still left

--- Exp_04/srp.c
+++ Exp_04/srp.c
@@ -9,74 +9,91 @@ B20CS1147
 #include <stdlib.h>
 #include <string.h>
 
-struct Productions {
-	char lhs[10];
-	char rhs[30];
-} gr[20];
+char* extract(char _input[30], int _start, int _end) {
+    char *result;
+    int _length = _end - _start + 1;
+    result = malloc((_length + 1) * sizeof(char));
 
+    int _pos = 0;
+    while (_pos < _length) {
+        result[_pos] = _input[_start + _pos];
+        _pos++;
+    }
+    result[_pos] = '\0';
+
+    return result;
+}
+
+char* appendDollarSymbol(char _input[30]) {
+	char *result;
+    int _length = strlen(_input);
+    result = malloc((_length + 1) * sizeof(char));
+
+	int i = 0;
+	while (i<strlen(_input)) {
+		result[i] = _input[i];
+		i++;
+	}
+	result[i] = '$';
+
+	return result;
+}
+
+// struct to hold grammar
 int prodCount = 0;
+struct Productions {
+	char left;
+	char right[30];
+} grammar[20];
 
-int top = 0;
+// stack operations
+int stk_top = 0;
 char stk[100];
 void push(char ch) {
-	stk[++top] = ch;
+	stk[stk_top++] = ch;
 }
 char pop() {
-	return stk[top--];
+	return stk[stk_top--];
 }
 
 int main() {
-	// input grammar
-	// input string
-	
 	printf("Input no of productions: "); scanf("%d", &prodCount);
 	
 	// input productions
-	printf("Input productions[without any spaces]:\n");
-	char inp[100];
+	printf("\nInput productions[without any spaces, like A->...]:\n");
+	char input[100];
 	for (int i=0; i<prodCount; i++) {
-		scanf("%s", inp);
+		scanf("%s", input);
 		
-		int index = 0;
-		int j;
-		for (j=0; j<strlen(inp); j++) {
-			if(inp[j] == ' ' || inp[j] == '-') {
-				// add to lhs \0, break
-				gr[i].lhs[index] = '\0';
-				break;
-			}
-			// add character to lhs
-			gr[i].lhs[index] = inp[j];
-			index++;
-		}
-		
-		index = 0;
-		for (; j<strlen(inp); j++) {
-			if (inp[j] == ' ' || inp[j] == '>') {
-				index++;
-				continue;
-			}
-			gr[i].rhs[index] = inp[j];
-			index++;
-		}
-		gr[i].rhs[index] = '\0';
+		grammar[i].left = input[0];
+		strcpy(grammar[i].right, extract(input, 3, strlen(input)));
 	}
 	
+	// input string
 	char str[100];
-	printf("Input string[append $ at end]: "); scanf("%s", str);
+	printf("Input string: "); scanf("%s", str);
+	strcpy(str, appendDollarSymbol(str));
+	int str_index = 0;	// to track position in str
 	
-	
-	
+	/*
+	consider stk like a string; add new characters to right side; check from right to left;
+	*/
 	// processes 
 	push('$');
-	
-	// loop through str from l->r and check
-	for (int i=0; i<strlen(str); i++) {
-		// shift, reduce if possible
+	// first, push a $symbol to stk to indicate empty stack
+	// is stk empty, or dont match, push element to stack, ie shift
+	// check if stack top ie right-most element matches RHS of production else take right two elements nd check. continue this till $ found.
+	while (stk[stk_top-1] != '$' && str[str_index] != '$') {
+
 	}
+
+
+	if (stk[stk_top-1] == '$' && stk_top == 1 && str[str_index] == '$')
+		printf("Parsing successful");
+	else 
+		printf("Parsing unsuccessful");
 	
-	
-	
+
 	return 0;
 }
 
