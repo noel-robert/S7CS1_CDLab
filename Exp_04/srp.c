bfe49aba4693c07bc1fcec667a9fc17c88432f7d
Noel Robert <noel060202@gmail.com>
2023-11-18 10:42:42 +0530

    read input grammar from file

--- Exp_04/srp.c
+++ Exp_04/srp.c
@@ -50,48 +50,139 @@ struct Productions {
 int stk_top = 0;
 char stk[100];
 void push(char ch) {
+	printf("pushed - %c\n", ch);
 	stk[stk_top++] = ch;
 }
 char pop() {
 	return stk[stk_top--];
 }
 
-int main() {
-	printf("Input no of productions: "); scanf("%d", &prodCount);
-	
-	// input productions
-	printf("\nInput productions[without any spaces, like A->...]:\n");
-	char input[100];
-	for (int i=0; i<prodCount; i++) {
-		scanf("%s", input);
-		
-		grammar[i].left = input[0];
-		strcpy(grammar[i].right, extract(input, 3, strlen(input)));
+// shift and reduce operations
+void shift(char input) {
+	push(input);
+}
+void reduce(int productionIndex) {
+	for (int i=0; i<strlen(grammar[productionIndex].right); i++) {
+		pop();
 	}
+
+	push(grammar[productionIndex].left);
+}
+
+int main() {
+
+	FILE *inputFile = fopen("grammar.txt", "r");
+	
+	char line[30];
+	int lineLen = 0;
+	char ch;
+
+	while ((ch = getc(inputFile)) != EOF) {
+		if (ch == '\n') {
+			grammar[prodCount].left = line[0];
+			strcpy(grammar[prodCount].right, extract(line, 3, lineLen-1));
+			prodCount++;
+
+			lineLen = 0;
+			continue;
+		}
+
+		line[lineLen++] = ch;
+	}
+	if (lineLen > 0) {
+		grammar[prodCount].left = line[0];
+		strcpy(grammar[prodCount].right, extract(line, 3, lineLen-1));
+		prodCount++;
+	}
+
 	
 	// input string
 	char str[100];
-	printf("Input string: "); scanf("%s", str);
+	printf("\nInput string: "); scanf("%s", str);
 	strcpy(str, appendDollarSymbol(str));
 	int str_index = 0;	// to track position in str
 	
-	/*
-	consider stk like a string; add new characters to right side; check from right to left;
-	*/
-	// processes 
+	// processes
 	push('$');
-	// first, push a $symbol to stk to indicate empty stack
-	// is stk empty, or dont match, push element to stack, ie shift
-	// check if stack top ie right-most element matches RHS of production else take right two elements nd check. continue this till $ found.
-	while (stk[stk_top-1] != '$' && str[str_index] != '$') {
 
+	while ((ch = str[str_index]) != '$') {
+		
+		if (stk[stk_top] == '$') {
+			// initial condition
+			shift(ch);
+			str_index++; // only do str_index++ on shifting
+			continue;
+		}
+
+		/*
+		check if reduction can be done on elements in stack
+		count no of possible reductions - 
+		- if zero, try shifting
+		- if one, reduce
+		- if > 1, reduce reduce conflict
+		*/
+		int possible_reductions = 0;
+		int reduction = -1;
+		for (int i=0; i<prodCount; i++) {
+			int len = strlen(grammar[i].right);
+
+			if ((stk_top-1 >= len) && (strcmp(extract(stk, stk_top-len, stk_top-1), grammar[i].right) == 0)) {
+				// reduction is possible
+				possible_reductions++;
+				reduction = i;
+				// also count no of possible reductions to find reduce-reduce conflict
+			}
+		}
+
+		if (possible_reductions == 0) {
+			shift(ch);
+			str_index++;
+			continue;
+		} else if (possible_reductions == 1) {
+			reduce(reduction);
+		} else {
+			printf("Reduce-Reduce conflict occurred\n");
+			break;
+		}
 	}
 
+	printf("%s\n", stk);
 
-	if (stk[stk_top-1] == '$' && stk_top == 1 && str[str_index] == '$')
-		printf("Parsing successful");
-	else 
-		printf("Parsing unsuccessful");
+
+
+
+
+
+
+
+
+
+
+
+
+
+	// some problem here, check condition individually
+	// if (stk[stk_top - 1] == '$' && stk_top == 1 && str[str_index] == '$') {
+	// 	printf("Parsing successful\n");
+	// } else {
+	// 	printf("Parsing unsuccessful\n");
+	// }
+	
+
+	// debugging
+	// if (!(stk[stk_top - 1] == '$')) {
+	// 	printf("stk[stk_top - 1] == '$'\n");
+	// } else if (!(stk_top == 1)) {
+	// 	printf("stk_top == 1\n");
+	// } else if (!(str[str_index] == '$')) {
+	// 	printf("%c\n", str[str_index]);
+	// } else {
+	// 	printf("Dunnno bruv\n");
+	// }
+
+	// for (int i = 0; i < prodCount; i++) {
+    // 	printf("Production %d: %c -> %s^&\n", i + 1, grammar[i].left, grammar[i].right);
+	// }
 	
 
 	return 0;
