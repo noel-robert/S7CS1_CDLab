d36124543c8d2bafc77c71b102d342dba1ea75c4
Noel Robert <noel060202@gmail.com>
2023-11-28 22:38:48 +0530

    modified code to get unique non-terminals from LHS

--- Exp_08/fandf.c
+++ Exp_08/fandf.c
@@ -15,12 +15,18 @@ int prodCount = 0;
 struct Productions {
     char left;
     char right[30];
+} grammar[50];
+
+int indexFF = 0;
+struct FirstFollow {
+	char left;
 
 	char first[100];
 	int first_index;
+
 	char follow[100];
 	int follow_index;
-} grammar[20];
+} arr[50];
 
 char* extract(char _input[30], int _start, int _end) {
     char *result;
@@ -57,21 +63,24 @@ void findFirst(char symbol) {
 	- two productions with same LHS means two different arrays for first and follow, along with symbol
 	- so, first and follow should be another struct 
 	*/
-	for (int i=0; i<prodCount; i++) {
-		if (grammar[i].left == symbol) {
-			char firstSymbol = grammar[i].right[0];
 
-			if (isTerminal(firstSymbol)) {
-				addToSet(grammar[i].first, firstSymbol, &grammar[i].first_index);
-			} else {
-				if (firstSymbol != '#' && firstSymbol != '\0') {
-					findFirst(firstSymbol);
-				} else if (firstSymbol == '#') {
-					addToSet(grammar[i].first, '#', &grammar[i].first_index);
-				}
-			}
-		}
-	}
+	// for (int i=0; i<prodCount; i++) {
+	// 	if (grammar[i].left == symbol) {
+	// 		char firstSymbol = grammar[i].right[0];
+
+	// 		if (isTerminal(firstSymbol)) {
+	// 			addToSet(grammar[i].first, firstSymbol, &grammar[i].first_index);
+	// 		} else {
+	// 			if (firstSymbol != '#' && firstSymbol != '\0') {
+	// 				findFirst(firstSymbol);
+	// 			} else if (firstSymbol == '#') {
+	// 				addToSet(grammar[i].first, '#', &grammar[i].first_index);
+	// 			}
+	// 		}
+	// 	}
+	// }
+
+
 }
 
 void findFollow(char symbol) {}
@@ -84,35 +93,71 @@ int main() {
 	char ch;
 
     // copy production rules to structure
-	while ((ch = getc(inputFile)) != EOF) {
-		if (ch == '\n') {
-			grammar[prodCount].left = line[0];
-			strcpy(grammar[prodCount].right, extract(line, 3, lineLen-1));
+	// while ((ch = getc(inputFile)) != EOF) {
+	// 	if (ch == '\n') {
+	// 		grammar[prodCount].left = line[0];
+	// 		strcpy(grammar[prodCount].right, extract(line, 3, lineLen-1));
+	// 		prodCount++;
 
-			grammar[prodCount].first_index = 0;
-			grammar[prodCount].follow_index = 0;
-			prodCount++;
+	// 		lineLen = 0;
+	// 		continue;
+	// 	}
 
-			lineLen = 0;
-			continue;
+	// 	line[lineLen++] = ch;
+	// }
+	// if (lineLen > 0) {
+	// 	grammar[prodCount].left = line[0];
+	// 	strcpy(grammar[prodCount].right, extract(line, 3, lineLen-1));
+	// 	prodCount++;
+	// }
+	do {
+		ch = getc(inputFile);
+
+		if (ch == '\n' || ch == EOF) {
+			if (lineLen > 0) {
+				grammar[prodCount].left = line[0];
+				strcpy(grammar[prodCount].right, extract(line, 3, lineLen-1));
+				prodCount++;
+
+				lineLen = 0;
+			}
+		} else {
+			line[lineLen++] = ch;
 		}
+	} while (ch != EOF);
 
-		line[lineLen++] = ch;
+
+	// iterate through each left in grammar[]. add to arr[] if not present in it 
+	for(int i=0; i<prodCount; i++) {
+		char ch = grammar[i].left;
+
+		// if (indexFF == 0) {
+		// 	arr[indexFF].left = ch;
+		// 	arr[indexFF].first_index = 0;
+		// 	arr[indexFF].follow_index = 0;
+		// 	indexFF++;
+		// 	continue;
+		// }
+
+		// check if ch already in arr[]
+		int found = 0;
+		for (int j=0; j<indexFF; j++) {
+			if (arr[j].left == ch) {
+				found = 1;
+				break;
+			}
+		}
+		if (found == 0) {
+			arr[indexFF].left = ch;
+			arr[indexFF].first_index = 0;
+			arr[indexFF].follow_index = 0;
+			indexFF++;
+		}
 	}
-	if (lineLen > 0) {
-		grammar[prodCount].left = line[0];
-		strcpy(grammar[prodCount].right, extract(line, 3, lineLen-1));
-
-		grammar[prodCount].first_index = 0;
-		grammar[prodCount].follow_index = 0;
-		prodCount++;
-	}
-
-
 
 	// find first for every LHS of productions
-	for (int i=0; i<prodCount; i++) {
-		findFirst(grammar[i].left);
+	for (int i=0; i<indexFF; i++) {
+		findFirst(arr[i].left);
 	}
 
 	// find follow for every LHS of productions
@@ -121,8 +166,8 @@ int main() {
 	// }
 
 
-	for (int i=0; i<prodCount; i++) {
-		printf("First(%c): %s\n", grammar[i].left, grammar[i].first);
+	for (int i=0; i<indexFF; i++) {
+		printf("First(%c): %s\n", arr[i].left, arr[i].first);
 		// printf("Follow(%c): %s\n", grammar[i].left, grammar[i].follow);
 		printf("\n");
 	}
