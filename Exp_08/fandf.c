7d69ff0ba73aecb4b589fd2c808de89e72759eb9
Noel Robert <noel060202@gmail.com>
2023-12-19 16:38:40 +0530

    minor change in variable name

--- Exp_08/fandf.c
+++ Exp_08/fandf.c
@@ -26,7 +26,7 @@ struct FirstFollow {
 
 	char follow[100];
 	int follow_index;
-} arr[50];
+} symbolData[50];
 
 char* extract(char _input[30], int _start, int _end) {
     char *result;
@@ -57,30 +57,30 @@ void addToSet(char set[], char symbol, int *index) {
 	set[(*index)++] = symbol;
 }
 
-void findFirst(char symbol) {
-	/*
-	Issue here:
-	- two productions with same LHS means two different arrays for first and follow, along with symbol
-	- so, first and follow should be another struct 
-	*/
+int getIndexOfSymbol(char symbol) {
+	for (int i=0; i<indexFF; i++) {
+		if (symbolData[i].left == symbol) return i;
+	}
+	printf("Fatal Error\n\n");
+	return -1;
+}
 
-	// for (int i=0; i<prodCount; i++) {
-	// 	if (grammar[i].left == symbol) {
-	// 		char firstSymbol = grammar[i].right[0];
+void findFirst(char symbol, int index) {
+	for (int i=0; i<prodCount; i++) {
+		if (grammar[i].left == symbol) {
+			char firstSymbol = grammar[i].right[0];
 
-	// 		if (isTerminal(firstSymbol)) {
-	// 			addToSet(grammar[i].first, firstSymbol, &grammar[i].first_index);
-	// 		} else {
-	// 			if (firstSymbol != '#' && firstSymbol != '\0') {
-	// 				findFirst(firstSymbol);
-	// 			} else if (firstSymbol == '#') {
-	// 				addToSet(grammar[i].first, '#', &grammar[i].first_index);
-	// 			}
-	// 		}
-	// 	}
-	// }
-
-
+			if (isTerminal(firstSymbol)) {
+				addToSet(symbolData[index].first, firstSymbol, &symbolData[index].first_index);
+			} else {
+				if (firstSymbol != '#' && firstSymbol != '\0') {
+					findFirst(firstSymbol, getIndexOfSymbol(firstSymbol));
+				} else if (firstSymbol == '#') {
+					addToSet(symbolData[index].first, '#', &symbolData[index].first_index);
+				}
+			}
+		}
+	}
 }
 
 void findFollow(char symbol) {}
@@ -93,23 +93,6 @@ int main() {
 	char ch;
 
     // copy production rules to structure
-	// while ((ch = getc(inputFile)) != EOF) {
-	// 	if (ch == '\n') {
-	// 		grammar[prodCount].left = line[0];
-	// 		strcpy(grammar[prodCount].right, extract(line, 3, lineLen-1));
-	// 		prodCount++;
-
-	// 		lineLen = 0;
-	// 		continue;
-	// 	}
-
-	// 	line[lineLen++] = ch;
-	// }
-	// if (lineLen > 0) {
-	// 	grammar[prodCount].left = line[0];
-	// 	strcpy(grammar[prodCount].right, extract(line, 3, lineLen-1));
-	// 	prodCount++;
-	// }
 	do {
 		ch = getc(inputFile);
 
@@ -127,49 +110,44 @@ int main() {
 	} while (ch != EOF);
 
 
-	// iterate through each left in grammar[]. add to arr[] if not present in it 
+	// iterate through each left in grammar[]. add to symbolData[] if not present in it 
 	for(int i=0; i<prodCount; i++) {
 		char ch = grammar[i].left;
 
-		// if (indexFF == 0) {
-		// 	arr[indexFF].left = ch;
-		// 	arr[indexFF].first_index = 0;
-		// 	arr[indexFF].follow_index = 0;
-		// 	indexFF++;
-		// 	continue;
-		// }
-
-		// check if ch already in arr[]
+		// check if ch already in symbolData[]
 		int found = 0;
 		for (int j=0; j<indexFF; j++) {
-			if (arr[j].left == ch) {
+			if (symbolData[j].left == ch) {
 				found = 1;
 				break;
 			}
 		}
 		if (found == 0) {
-			arr[indexFF].left = ch;
-			arr[indexFF].first_index = 0;
-			arr[indexFF].follow_index = 0;
+			symbolData[indexFF].left = ch;
+			symbolData[indexFF].first_index = 0;
+			symbolData[indexFF].follow_index = 0;
 			indexFF++;
 		}
 	}
 
 	// find first for every LHS of productions
 	for (int i=0; i<indexFF; i++) {
-		findFirst(arr[i].left);
+		findFirst(symbolData[i].left, i);
 	}
 
 	// find follow for every LHS of productions
-	// for (int i=0; i<prodCount; i++) {
-	// 	findFollow(grammar[i].left);
+	// for (int i=0; i<indexFF; i++) {
+	// 	findFollow(symbolData[i].left);
 	// }
 
 
 	for (int i=0; i<indexFF; i++) {
-		printf("First(%c): %s\n", arr[i].left, arr[i].first);
-		// printf("Follow(%c): %s\n", grammar[i].left, grammar[i].follow);
-		printf("\n");
+		// printf("First(%c): %s\n", symbolData[i].left, symbolData[i].first);
+		printf("First(%c): { %c", symbolData[i].left, symbolData[i].first[0]);
+		if (strlen(symbolData[i].first) > 1) {
+			for (int j=1; j<strlen(symbolData[i].first); j++) printf(", %c", symbolData[i].first[j]);
+		}
+		printf(" }\n");
 	}
 
 	return 0;
